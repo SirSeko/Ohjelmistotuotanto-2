756dffc17b9d5afffc79d6738836073994a669db
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-02-04 13:30:37 +0200

    Tehty pohja C# ohjelmistolle.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace varastosovellus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
