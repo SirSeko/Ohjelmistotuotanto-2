05019f78f21c8939fa8f626c4af331ad8c069fb5
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-02-22 15:56:40 +0200

    Kayttaja-olio ilman virheilmoituksia

---
+++ VarastoApi/VarastoApi/Backend/Kayttaja.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VarastoApi.Backend {
+    //Käyttäjä-olio, kertoo käyttäjänimen ja valtuustason
+    public class Kayttaja {
+        public string Kayttajanimi;
+        public int Valtuus;
+
+        public Kayttaja(string kayttajanimi, int valtuus) {
+            if (checkKayttajanimi(kayttajanimi)) {
+                this.Kayttajanimi = kayttajanimi;
+            } else {
+                //TODO Virheilmoitus
+            }
+            if (checkValtuus(valtuus)) {
+                this.Valtuus = valtuus;
+            } else {
+                //TODO Virheilmoitus
+            }
+        }
+
+        bool checkValtuus(int valtuus) {
+            if (valtuus < 0 || valtuus > 2) return false;
+            else return true;
+        }
+
+        bool checkKayttajanimi(string kayttajanimi) {
+            if (kayttajanimi.Length < 4) return false;
+            else return true;
+        }
+    }
+}
