087a620ba67ea1a741915ec6d8e6281e972c4b18
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-02-22 20:24:40 +0200

    Tilaus.cs virheilmoitukset ja factory method pattern.

--- VarastoApi/VarastoApi/Backend/Tilaus.cs
+++ VarastoApi/VarastoApi/Backend/Tilaus.cs
@@ -1,7 +1,5 @@
-namespace VarastoApi.Backend
-{
-    public class Tilaus
-    {
+namespace VarastoApi.Backend {
+    public class Tilaus {
         public int Id;
         public string TilaajanNimi;
         public int Maara;
@@ -10,79 +8,75 @@ namespace VarastoApi.Backend
 
 
         //Uudet tilaukset luodaan vain konstruktorilla!! Näin toimii hyvä tarkistus
-        public Tilaus(int Id, string TilaajanNimi, int Maara, int MateriaaliId)
-        {
-            if (checkId(Id)){
-                this.Id = Id;
-            } else {
-                //TODO Virheilmoitus
+        private Tilaus(int Id, string TilaajanNimi, int Maara, int MateriaaliId) {
+            this.Id = Id;
+            this.TilaajanNimi = TilaajanNimi;
+            this.Maara = Maara;
+            this.MateriaaliId = MateriaaliId;
             }
-            if (checkTilaajanNimi(TilaajanNimi)){
-                this.TilaajanNimi = TilaajanNimi;
-            } else {
-                //TODO Virheilmoitus
+
+        /// <summary>
+        /// Tarkistaa parametrit ja luo uuden tilauksen. Viheellisellä syötteellä palauttaa null.
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="TilaajanNimi"></param>
+        /// <param name="Maara"></param>
+        /// <param name="MateriaaliId"></param>
+        /// <returns>Tilaus || null</returns>
+        public static Tilaus Create(int Id, string TilaajanNimi, int Maara, int MateriaaliId) {
+            Tilaus t = new Tilaus(-1, "", -1, -1);
+            if (!checkId(Id)) {
+                ExceptionController.WriteException(t, "Tilasta luodessa huono ID.");
+                return null;
             }
-            if (checkMaara(Maara)){
-                this.Maara = Maara;
-            } else {
-                //TODO Virheilmoitus
+            if (!checkTilaajanNimi(TilaajanNimi)) {
+                ExceptionController.WriteException(t, "Tilausta luodessa huono tilaajan nimi.");
+                return null;
             }
-            if (checkMateriaaliId(MateriaaliId)){
-                this.MateriaaliId = MateriaaliId;
-            } else {
-                //TODO Virheilmoitus
+            if (!checkMaara(Maara)) {
+                ExceptionController.WriteException(t, "Tilausta luodessa huono tilaajan nimi.");
+                return null;
             }
+            if (!checkMateriaaliId(MateriaaliId)) {
+                ExceptionController.WriteException(t, "Tilausta luodessa huono materiaalin id.");
+                return null;
+            }
+            return new Tilaus(Id, TilaajanNimi, Maara, MateriaaliId);
         }
 
 
         //tarkistus ID:lle TODO
-        bool checkId(int Id)
-        {
-            if (Id < -1)
-            {
+        static bool checkId(int Id) {
+            if (Id < -1) {
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
 
         //tarkistus tilaajan nimelle, lähinnä ettei ole tyhjä ja on enemmän kuin vaikka "x"
-        bool checkTilaajanNimi(string TilaajanNimi)
-        {
-            if (TilaajanNimi == null || TilaajanNimi == "" || TilaajanNimi.Length < 2)
-            {
+        static bool checkTilaajanNimi(string TilaajanNimi) {
+            if (TilaajanNimi == null || TilaajanNimi == "" || TilaajanNimi.Length < 2) {
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
 
         //tarkistus määrälle
-        bool checkMaara(int Maara)
-        {
-            if (Maara < 0)
-            {
+        static bool checkMaara(int Maara) {
+            if (Maara < 0) {
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
 
         //tarkistus Materiaalin ID:lle TODO
-        bool checkMateriaaliId(int Id)
-        {
-            if (Id < 0)
-            {
+        static bool checkMateriaaliId(int Id) {
+            if (Id < 0) {
                 return false;
-            }
-            else
-            {
+            } else {
                 return true;
             }
         }
