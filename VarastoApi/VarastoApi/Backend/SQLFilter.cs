46f32c6e570c6ba890f8f20d403ca458e9b5b48a
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-02-22 12:47:49 +0200

    SQLFIlter käyttäjän syötteen tarkistamiseen.

---
+++ VarastoApi/VarastoApi/Backend/SQLFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VarastoApi.Backend {
+
+
+    //Tarkistaa käyttäjän syötteen sisällön, ettei sieltä löydy mitään sisältöä, jolla voisi vahingoittaa tietokantaa
+    public class SQLFilter {
+        string[] badInput = { ";", "'", "--", "/*", "*/", "xp_" };
+        public SQLFilter(string input) {
+        }
+
+        public bool checkInput(string input) {
+            foreach (string s in badInput) {
+                if (input.Contains(s)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
