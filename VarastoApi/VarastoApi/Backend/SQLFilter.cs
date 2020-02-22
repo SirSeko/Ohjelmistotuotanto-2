6888a7d4609ccc7e003799b9b907c23ff3fef059
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-02-22 15:02:11 +0200

    ohjekommentit SQLFilteriin

--- VarastoApi/VarastoApi/Backend/SQLFilter.cs
+++ VarastoApi/VarastoApi/Backend/SQLFilter.cs
@@ -6,12 +6,14 @@ using System.Web;
 namespace VarastoApi.Backend {
 
 
-    //Tarkistaa käyttäjän syötteen sisällön, ettei sieltä löydy mitään sisältöä, jolla voisi vahingoittaa tietokantaa
+    //Tarkistaa käyttäjän syötteen sisällön, ettei sieltä löydy mitään sisältöä, jolla voisi vahingoittaa tietokantaa. Olio kannattanee luoda kontrollerissa
     public class SQLFilter {
         string[] badInput = { ";", "'", "--", "/*", "*/", "xp_" };
         public SQLFilter(string input) {
         }
 
+        //tarkistetaan KAIKKI käyttäjän input stringit joita käytetään tietokannassa, esim kaikki tallennukset ja muokkaukset
+        //Palauttaa false, jos kiellettyjä merkintöjä syötteessä, true muuten.
         public bool checkInput(string input) {
             foreach (string s in badInput) {
                 if (input.Contains(s)) {
