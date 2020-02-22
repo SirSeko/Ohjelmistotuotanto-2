321a1ac463839cb42a7a1e6371e7f43d331e1d0f
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-02-22 15:30:32 +0200

    SQLFilter muutettu staattiseksi luokaksi

--- VarastoApi/VarastoApi/Backend/SQLFilter.cs
+++ VarastoApi/VarastoApi/Backend/SQLFilter.cs
@@ -6,15 +6,13 @@ using System.Web;
 namespace VarastoApi.Backend {
 
 
-    //Tarkistaa käyttäjän syötteen sisällön, ettei sieltä löydy mitään sisältöä, jolla voisi vahingoittaa tietokantaa. Olio kannattanee luoda kontrollerissa
-    public class SQLFilter {
-        string[] badInput = { ";", "'", "--", "/*", "*/", "xp_" };
-        public SQLFilter(string input) {
-        }
+    //Tarkistaa käyttäjän syötteen sisällön, ettei sieltä löydy mitään sisältöä, jolla voisi vahingoittaa tietokantaa.
+    public static class SQLFilter {
+        static string[] badInput = { ";", "'", "--", "/*", "*/", "xp_" };
 
         //tarkistetaan KAIKKI käyttäjän input stringit joita käytetään tietokannassa, esim kaikki tallennukset ja muokkaukset
         //Palauttaa false, jos kiellettyjä merkintöjä syötteessä, true muuten.
-        public bool checkInput(string input) {
+        public static bool checkInput(string input) {
             foreach (string s in badInput) {
                 if (input.Contains(s)) {
                     return false;
