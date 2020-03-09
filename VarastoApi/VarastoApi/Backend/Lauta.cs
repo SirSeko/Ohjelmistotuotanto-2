9f0f66ea4eb9ee446bc490ed7f5c1504fadf0a57
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-03-09 22:52:23 +0200

    Luotu Lauta.cs

---
+++ VarastoApi/VarastoApi/Backend/Lauta.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VarastoApi.Backend {
+    public class Lauta {
+        public int Id;
+        public string Koko;
+        public float Hinta;
+        public int Maara;
+        public string Yksikko;
+        public int Sijainti;
+        public string Kauppa;
+        public string Lisatiedot;
+
+        private Lauta(int id, string koko, float hinta, int maara, string yksikko, int sijainti, string kauppa, string lisatiedot) {
+            Id = id;
+            Koko = koko;
+            Hinta = hinta;
+            Maara = maara;
+            Yksikko = yksikko;
+            Sijainti = sijainti;
+            Kauppa = kauppa;
+            Lisatiedot = lisatiedot;
+        }
+
+        public Lauta Create(int id, string koko, float hinta, int maara, string yksikko, int sijainti, string kauppa, string lisatiedot) {
+            if (checkHinta(hinta) && checkId(id) && checkKauppa(kauppa) && checkKoko(koko) && checkLisatiedot(lisatiedot) && checkMaara(maara) && checkSijainti(sijainti) && checkYksikko(yksikko)) {
+                return new Lauta(id, koko, hinta, maara, yksikko, sijainti, kauppa, lisatiedot);
+            } else return null;
+        }
+
+        bool checkId(int id) {
+            if (id < -1) {
+                ExceptionController.WriteException(this, "Virhe ID:ssä.");
+                return false;
+            } else return true;
+        }
+
+        bool checkKoko(string koko) {
+            if (koko.Length > 45 || koko == null || koko == "") {
+                ExceptionController.WriteException(this, "Virhe koossa.");
+                return false;
+            } else return true;
+        }
+
+        bool checkHinta(float hinta) {
+            if (hinta < 0.0f) {
+                ExceptionController.WriteException(this, "Virhe hinnassa.");
+                return false;
+            } else return true;
+        }
+
+        bool checkMaara(int maara) {
+            if (maara < -1) {
+                ExceptionController.WriteException(this, "Virhe määrässä.");
+                return false;
+            } else return true;
+        }
+
+        bool checkYksikko(string yksikko) {
+            if (yksikko.Length > 45 || yksikko == null || yksikko == "") {
+                ExceptionController.WriteException(this, "Virhe yksikössä.");
+                return false;
+            } else return true;
+        }
+
+        bool checkSijainti(int sijainti) {
+            if (sijainti < -1) {
+                ExceptionController.WriteException(this, "Virhe sijainnissa.");
+                return false;
+            } else return true;
+        }
+
+        bool checkKauppa(string kauppa) {
+            if (kauppa.Length > 50) {
+                ExceptionController.WriteException(this, "Kaupan nimi liian pitkä.");
+                return false;
+            } else return true;
+        }
+
+        bool checkLisatiedot(string lisatiedot) {
+            if (lisatiedot.Length > 100) {
+                ExceptionController.WriteException(this, "Lisatiedoissa liikaa merkkejä.");
+                return false;
+            } else return true;
+        }
+    }
+}
