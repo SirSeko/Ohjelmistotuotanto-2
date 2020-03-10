795af2227b12af8603647e3dd1b795a4fd3dfaf7
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-03-10 18:20:26 +0200

    MateriaaliKoonti-dummy

---
+++ VarastoApi/VarastoApi/Backend/MateriaaliKoonti.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VarastoApi.Backend {
+    public class MateriaaliKoonti {
+        public List<Materiaali> Materiaalit;
+        public List<Vaneri> Vanerit;
+        public List<Sijainti> Sijainnit;
+        public List<Maali> Maalit;
+        public List<Lauta> Laudat;
+    }
+}
