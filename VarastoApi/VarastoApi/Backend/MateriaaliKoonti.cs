8936a99b68e3b7eb447c4279916ea28befd71f83
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-03-10 18:22:06 +0200

    MateriaaliKoonti konstruktori

--- VarastoApi/VarastoApi/Backend/MateriaaliKoonti.cs
+++ VarastoApi/VarastoApi/Backend/MateriaaliKoonti.cs
@@ -10,5 +10,13 @@ namespace VarastoApi.Backend {
         public List<Sijainti> Sijainnit;
         public List<Maali> Maalit;
         public List<Lauta> Laudat;
+
+        public MateriaaliKoonti() {
+            Materiaalit = new List<Materiaali>();
+            Vanerit = new List<Vaneri>();
+            Sijainnit = new List<Sijainti>();
+            Maalit = new List<Maali>();
+            Laudat = new List<Lauta>();
+        }
     }
 }
