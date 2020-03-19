72f4dfe78844ba2e0f9faae4c7bb4cd187ff4293
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-03-19 14:35:36 +0200

    Jotain fixattu testille homecontrollerissa

--- VarastoApi/VarastoApi/Backend/MateriaaliKoonti.cs
+++ VarastoApi/VarastoApi/Backend/MateriaaliKoonti.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace VarastoApi.Backend {
     public class MateriaaliKoonti {
@@ -11,12 +12,42 @@ namespace VarastoApi.Backend {
         public List<Maali> Maalit;
         public List<Lauta> Laudat;
 
+        private DatabaseManager dbMan;
+        private DatabaseMateriaali dbMat;
+        private DatabaseLauta dbLau;
+        private DatabaseMaali dbMaa;
+        private DatabaseVaneri dbVan;
+        private DatabaseSijainti dbSij;
+
+        SqlConnection cnn;
+
         public MateriaaliKoonti() {
             Materiaalit = new List<Materiaali>();
             Vanerit = new List<Vaneri>();
             Sijainnit = new List<Sijainti>();
             Maalit = new List<Maali>();
             Laudat = new List<Lauta>();
+
+            dbMan = new DatabaseManager();
+            cnn = dbMan.OpenConnection();
+
+            dbMat = new DatabaseMateriaali(cnn);
+            dbLau = new DatabaseLauta(cnn);
+            dbMaa = new DatabaseMaali(cnn);
+            dbVan = new DatabaseVaneri(cnn);
+            dbSij = new DatabaseSijainti(cnn);
+
+            initiate();
+            dbMan.CloseConnection();
+            
+        }
+
+        void initiate() {
+            Materiaalit = dbMat.SelectAll(Materiaalit);
+            Vanerit = dbVan.SelectAll(Vanerit);
+            Sijainnit = dbSij.SelectAll(Sijainnit);
+            Maalit = dbMaa.SelectAll(Maalit);
+            Laudat = dbLau.SelectAll(Laudat);
         }
     }
 }
--- VarastoApi/VarastoApi/Controllers/HomeController.cs
+++ VarastoApi/VarastoApi/Controllers/HomeController.cs
@@ -22,8 +22,9 @@ namespace VarastoApi.Controllers
     public class HomeController : Controller
     {
 
-        List<Vaneri> vaneri = new List<Vaneri>();
+        List<Vaneri> vanerit = new List<Vaneri>();
         SqlConnection cnn; //tietokantayhteys-olio, jaetaan tämä muualle
+        MateriaaliKoonti matko = new MateriaaliKoonti();
 
         public ActionResult Index()
         {
@@ -42,10 +43,10 @@ namespace VarastoApi.Controllers
             if (cnn != null)
             {
                 DatabaseVaneri dmVan = new DatabaseVaneri(cnn); //luodaan olio, jolla käsitellään materiaaleja tietokannassa
-                vaneri = dmVan.SelectAll(vaneri); //haetaan kaikki materiaalit kannasta listaan
+                vanerit = dmVan.SelectAll(vanerit); //haetaan kaikki materiaalit kannasta listaan
                 DatabaseMaali dmMaa = new DatabaseMaali(cnn); // sama maaleille
                 maali = dmMaa.SelectAll(maali);
-                mk.Vanerit = vaneri; //Lisätään vanerit koontilistaan
+                mk.Vanerit = vanerit; //Lisätään vanerit koontilistaan
                 mk.Maalit = maali; //Lisätään maalit koontilistaan
 
                 // SAMALLA PERIAATTEELLA VOI LISÄTÄ MUUT MATERIAALIT //
@@ -64,24 +65,17 @@ namespace VarastoApi.Controllers
 
 
         [HttpGet]
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int id)  //Edit napilla palauttaa LISTAN???????????? edit.cshtml:ään
         {
-            List<Vaneri> ID = new List<Vaneri>();
-
-            DatabaseManager dm = new DatabaseManager(); //tietokannanhallinta-olio, mm. yhdistys kantaan
-
-            cnn = dm.OpenConnection(); //yhdistetään kantaan
-            if (cnn != null)
-            {
-                DatabaseVaneri dmVan = new DatabaseVaneri(cnn); //luodaan olio, jolla käsitellään materiaaleja tietokannassa
-                ID = dmVan.SelectId(ID, id); //haetaan vaneri joiden ID == id
-                
-               
-
-
+            vanerit = matko.Vanerit;
+            foreach (Vaneri v in vanerit) {
+                if (v.Id == id) {
+                    List<Vaneri> PerkeleenPaskaListaHelvettiin = new List<Vaneri>();
+                    PerkeleenPaskaListaHelvettiin.Add(v);
+                    return PartialView(PerkeleenPaskaListaHelvettiin);
+                }
             }
-            return PartialView(ID);
-            
+            return PartialView(null); //nullll
         }
         
     }
