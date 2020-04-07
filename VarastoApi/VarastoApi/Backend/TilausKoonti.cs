59addda48c2259b7e1e3327ccbc8457354a76bf5
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-04-07 09:29:00 +0300

    Tilauskoontiin haku materiaaleille

--- VarastoApi/VarastoApi/Backend/TilausKoonti.cs
+++ VarastoApi/VarastoApi/Backend/TilausKoonti.cs
@@ -25,6 +25,36 @@ namespace VarastoApi.Backend {
             Tilaukset = dbTila.SelectAll(Tilaukset);
             dbMana.CloseConnection();
         }
+
+        //Hakee kannasta tilaukseen kuuluvat tilattavat ja lisää listaan(object), jonka palauttaa
+        public List<object> TilattavatMateriaalit(int TilausId) {
+            cnn = dbMana.OpenConnection();
+            DatabaseTilattava dbT = new DatabaseTilattava(cnn);
+            DatabaseLauta dbL = new DatabaseLauta(cnn);
+            DatabaseVaneri dbV = new DatabaseVaneri(cnn);
+            DatabaseMaali dbM = new DatabaseMaali(cnn);
+            List<object> tilMat = new List<object>();
+            List<Tilattava> tilattavat = dbT.SelectTilaus(TilausId);
+            if (tilattavat != null) {
+                foreach (Tilattava t in tilattavat) {
+                    object o = new object();
+                    if (o == null) {
+                        o = dbL.SelectId(t.MateriaaliId);
+                    }
+                    if (o == null) {
+                        o = dbV.SelectId(t.MateriaaliId);
+                    }
+                    if (o == null) {
+                        o = dbM.SelectId(t.MateriaaliId);
+                    }
+                    if (o != null) {
+                        tilMat.Add(o);
+                    }
+                }
+            }
+            dbMana.CloseConnection();
+            return tilMat;
+        }
     }
     
 }
