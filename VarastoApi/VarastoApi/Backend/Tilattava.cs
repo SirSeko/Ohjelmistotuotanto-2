8b7f0abcb1804be4fe9d0a0d61eb9ab53b53f150
Leo <leo.qvarnstrom@edu.savonia.fi>
2020-04-20 14:36:13 +0300

    Päivitetty DbTilattava.cs ja Tilattava.cs uuden taulun mukaiseksi.

--- VarastoApi/VarastoApi/Backend/Tilattava.cs
+++ VarastoApi/VarastoApi/Backend/Tilattava.cs
@@ -7,15 +7,17 @@ namespace VarastoApi.Backend {
     public class Tilattava {
         public int TilausId;
         public int MateriaaliId;
+        public int Maara;
 
-        public Tilattava(int TilausId, int MateriaaliId) {
+        public Tilattava(int TilausId, int MateriaaliId, int Maara) {
             this.TilausId = TilausId;
             this.MateriaaliId = MateriaaliId;
+            this.Maara = Maara;
         }
-        public static Tilattava Create(int TilausId, int MateriaaliId)
+        public static Tilattava Create(int TilausId, int MateriaaliId, int Maara)
         {
-            Tilattava t = new Tilattava(-1,-1);
-            return new Tilattava(TilausId, MateriaaliId);
+            Tilattava t = new Tilattava(-1,-1,-1);
+            return new Tilattava(TilausId, MateriaaliId, Maara);
         }
     }
 }
